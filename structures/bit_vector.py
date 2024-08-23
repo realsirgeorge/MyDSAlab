8188df9ed94a8e3f73b9fa815ce7aca6dfd87c54
SIR_GEORGE <89633633+realsirgeorge@users.noreply.github.com>
2024-08-23 13:35:16 +0300

    Update bit_vector.py

--- structures/bit_vector.py
+++ structures/bit_vector.py
@@ -3,225 +3,235 @@ Skeleton for COMP3506/7505 A1, S2, 2024
 The University of Queensland
 Joel Mackenzie and Vladimir Morozov
 """
-from typing import Any
-from structures.dynamic_array import DynamicArray
-class BitVector:
+from __future__ import annotations
+from typing import Any, Optional
+class Node:
     """
-    A compact storage for bits that uses DynamicArray under the hood.
-    Each element stores up to 64 bits, making BitVector 64 times more memory-efficient
-    for storing bits than plain DynamicArray.
+    A simple type to hold data and a next pointer
     """
-    BITS_PER_ELEMENT = 64
+    def __init__(self, data: Any) -> None:
+        self._data = data  # This is the payload data of the node
+        self._next = None  # This is the "next" pointer to the next Node
+        self._prev = None  # This is the "previous" pointer to the previous Node
+
+    def set_data(self, data: Any) -> None:
+        self._data = data
+
+    def get_data(self) -> Any:
+        return self._data
+
+    def set_next(self, node: Node) -> None:
+        self._next = node
+
+    def get_next(self) -> Node | None:
+        return self._next
+
+    def set_prev(self, node: Node) -> None:
+        self._prev = node
+
+    def get_prev(self) -> Node | None:
+        return self._prev
+
+class DoublyLinkedList:
+    """
+    Your doubly linked list code goes here.
+    Note that any time you see `Any` in the type annotations,
+    this refers to the "data" stored inside a Node.
+
+    [V3: Note that this API was changed in the V3 spec] 
+    """
 
     def __init__(self) -> None:
-        """
-        We will use the dynamic array as our data storage mechanism
-        """
-        self._data = DynamicArray()
-        self._size = 0  # Total number of bits in the vector
-        self._start_index = 0  # Initialize the start index for logical indexing
-        self._flipped = False  # Flag to indicate if the bits are logically flipped
-        self._reversed = False  # Flag to indicate if the vector is logically reversed
+        self.head: Optional[Node] = None
+        self.tail: Optional[Node] = None
+        self.size: int = 0
+        self.is_reversed: bool = False  # Initialize the is_reversed attribute
 
     def __str__(self) -> str:
         """
-        A helper that allows you to print a BitVector type
+        A helper that allows you to print a DoublyLinkedList type
         via the str() method.
         """
-        result = ""
-        for i in range(self._size):
-            result += "1" if self.get_at(i) else "0"
-        return result
+        if self.is_reversed:
+            current = self.tail
+            nodes = []
+            while current is not None:
+                nodes.append(str(current.get_data()))
+                current = current.get_prev()
+        else:
+            current = self.head
+            nodes = []
+            while current is not None:
+                nodes.append(str(current.get_data()))
+                current = current.get_next()
+        
+        return " <-> ".join(nodes)
 
-    def __resize(self) -> None:
-        pass
-
-    def _ensure_capacity(self, index: int):
-        """Ensure the bit vector has enough capacity to handle the given index."""
-        required_size = (index // self.BITS_PER_ELEMENT) + 1
-        while self._data.get_size() < required_size:
-            self._data.append(0)  # Initialize with 0 to avoid NoneType errors
-    
-    def get_at(self, index: int) -> int | None:
+    """
+    Simple Getters and Setters below
+    """
+    def get_size(self) -> int:
         """
-        Get bit at the given index.
-        Return None if index is out of bounds.
+        Return the size of the list.
         Time complexity for full marks: O(1)
         """
-        if index < 0 or index >= self._size:
+        return self.size
+
+    def get_head(self) -> Any | None:
+        """
+        Return the data of the leftmost node in the list, if it exists.
+        Time complexity for full marks: O(1)
+        """
+        if self.head is None:
             return None
-        # Adjust the index based on whether the vector is reversed
-        if self._reversed:
-            index = self._size - 1 - index
-        # Calculate the logical index taking the start index into account
-        logical_index = (self._start_index + index) % (self.BITS_PER_ELEMENT * self._data.get_size())
-        element_index = logical_index // self.BITS_PER_ELEMENT
-        bit_index = logical_index % self.BITS_PER_ELEMENT
-        bit_value = (self._data[element_index] >> bit_index) & 1
-        # If the vector is logically flipped, invert the bit value
-        return bit_value ^ self._flipped
+        return self.head.get_data()
 
-    def __getitem__(self, index: int) -> int | None:
+    def set_head(self, data: Any) -> None:
         """
-        Same as get_at.
-        Allows to use square brackets to index elements.
-        """
-        return self.get_at(index)
-
-    def set_at(self, index: int) -> None:
-        """
-        Set bit at the given index to 1.
-        Do not modify the vector if the index is out of bounds.
+        Replace the leftmost node's data with the given data.
+        If the list is empty, do nothing.
         Time complexity for full marks: O(1)
         """
-        if index < 0:
-            return
-        self._ensure_capacity(index)
+        if self.head is not None:
+            self.head.set_data(data)       
 
-        # Adjust the index based on whether the vector is reversed
-        if self._reversed:
-            index = self._size - 1 - index
-
-        logical_index = (self._start_index + index) % (self.BITS_PER_ELEMENT * self._data.get_size())
-        element_index = logical_index // self.BITS_PER_ELEMENT
-        bit_index = logical_index % self.BITS_PER_ELEMENT
-
-        if self._flipped:
-            # If the vector is logically flipped, we need to clear the bit
-            self._data[element_index] &= ~(1 << bit_index)
-        else:
-            # Normal operation: set the bit
-            self._data[element_index] |= (1 << bit_index)
-
-    def unset_at(self, index: int) -> None:
+    def get_tail(self) -> Any | None:
         """
-        Set bit at the given index to 0.
-        Do not modify the vector if the index is out of bounds.
+        Return the data of the rightmost node in the list, if it exists.
         Time complexity for full marks: O(1)
         """
-        if index < 0 or index >= self._size:
-            return
+        if self.tail is None:
+            return None
+        return self.tail.get_data()
 
-        # Adjust the index based on whether the vector is reversed
-        if self._reversed:
-            index = self._size - 1 - index
-        logical_index = (self._start_index + index) % (self.BITS_PER_ELEMENT * self._data.get_size())
-        element_index = logical_index // self.BITS_PER_ELEMENT
-        bit_index = logical_index % self.BITS_PER_ELEMENT
-        if self._flipped:
-            # If the vector is logically flipped, we need to set the bit
-            self._data[element_index] |= (1 << bit_index)
-        else:
-            # Normal operation: clear the bit
-            self._data[element_index] &= ~(1 << bit_index)
-
-    def __setitem__(self, index: int, state: int) -> None:
+    def set_tail(self, data: Any) -> None:
         """
-        Set bit at the given index.
-        Treat the integer in the same way Python does:
-        if state is 0, set the bit to 0, otherwise set the bit to 1.
-        Do not modify the vector if the index is out of bounds.
+        Replace the rightmost node's data with the given data.
+        If the list is empty, do nothing.
         Time complexity for full marks: O(1)
         """
-        if state == 0:
-            self.unset_at(index)
+        if self.tail is not None:
+            self.tail.set_data(data)
+
+    """
+    More interesting functionality now.
+    """
+
+    def insert_to_front(self, data: Any) -> None:
+        """
+        Insert the given data to the front of the list.
+        Hint: You will need to create a Node type containing
+        the given data.
+        Time complexity for full marks: O(1)
+        """
+        # if self.is_reversed:
+        #     self.insert_to_back(data)
+        # else:
+            # Original implementation for inserting at the front
+        new_node = Node(data)
+        if self.head is None:
+                self.head = new_node
+                self.tail = new_node
         else:
-            self.set_at(index)
+                new_node.set_next(self.head)
+                self.head.set_prev(new_node)
+                self.head = new_node
+        self.size += 1
 
-    def append(self, state: int) -> None:
+    def insert_to_back(self, data: Any) -> None:
         """
-        Add a bit to the back of the vector.
-        Treat the integer in the same way Python does:
-        if state is 0, set the bit to 0, otherwise set the bit to 1.
-        Time complexity for full marks: O(1*)
+        Insert the given data (in a node) to the back of the list
+        Time complexity for full marks: O(1)
         """
-        if self._size % self.BITS_PER_ELEMENT == 0:
-            self._data.append(0)
-        self._size += 1
-        self.__setitem__(self._size - 1, state)
+        # if self.is_reversed:
+        #     self.insert_to_front(data)
+        # else:
+            # Original implementation for inserting at the back
+        new_node = Node(data)
+        if self.tail is None:
+                self.head = new_node
+                self.tail = new_node
+        else:
+                new_node.set_prev(self.tail)
+                self.tail.set_next(new_node)
+                self.tail = new_node
+        self.size += 1
 
-    def prepend(self, state: Any) -> None:
+    def remove_from_front(self) -> Any | None:
         """
-        Add a bit to the front of the vector.
-        Treat the integer in the same way Python does:
-        if state is 0, set the bit to 0, otherwise set the bit to 1.
-        Time complexity for full marks: O(1*)
+        Remove the front node, and return the data it holds.
+        Time complexity for full marks: O(1)
         """
-        # Increase size of the bit vector
-        self._size += 1
-        # Ensure the bit vector has enough capacity
-        self._ensure_capacity(self._size - 1)
-        # Adjust the logical start position by moving it left
-        # If _start_index goes below 0, wrap it around using modulo operation
-        self._start_index = (self._start_index - 1) % (self.BITS_PER_ELEMENT * self._data.get_size())
-        # Set the first element in the logical bit vector to the new state
-        self.__setitem__(0, state)
+        if self.head is None:
+            return None
+        data = self.head.get_data()
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.get_next()
+            self.head.set_prev(None)
+        self.size -= 1
+        return data
+
+    def remove_from_back(self) -> Any | None:
+        """
+        Remove the back node, and return the data it holds.
+        Time complexity for full marks: O(1)
+        """
+        if self.tail is None:
+            return None
+        data = self.tail.get_data()
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.get_prev()
+            self.tail.set_next(None)
+        self.size -= 1
+        return data
+
+    def find_element(self, elem: Any) -> bool:
+        """
+        Looks at the data inside each node of the list and returns True
+        if a match is found; False otherwise.
+        Time complexity for full marks: O(N)
+        """
+        current = self.head
+        while current is not None:
+            if current.get_data() == elem:
+                return True
+            current = current.get_next()
+        return False
+
+    def find_and_remove_element(self, elem: Any) -> bool:
+        """
+        Looks at the data inside each node of the list; if a match is
+        found, this node is removed from the linked list, and True is returned.
+        False is returned if no match is found.
+        Time complexity for full marks: O(N)
+        """
+        current = self.head
+        while current is not None:
+            if current.get_data() == elem:
+                if current.get_prev() is not None:
+                    current.get_prev().set_next(current.get_next())
+                else:
+                    self.head = current.get_next()
+
+                if current.get_next() is not None:
+                    current.get_next().set_prev(current.get_prev())
+                else:
+                    self.tail = current.get_prev()
+
+                self.size -= 1
+                return True
+            current = current.get_next()
+        return False
 
     def reverse(self) -> None:
         """
-        Reverse the bit-vector.
+        Reverses the linked list
         Time complexity for full marks: O(1)
         """
-        self._reversed = not self._reversed
-
-    def flip_all_bits(self) -> None:
-        """
-        Flip all bits in the vector.
-        Time complexity for full marks: O(1)
-        """
-        self._flipped = not self._flipped
-
-    def shift(self, dist: int) -> None:
-        """
-        Make a bit shift.
-        If dist is positive, perform a left shift by `dist`.
-        Otherwise perform a right shift by `dist`.
-        Time complexity for full marks: O(N)
-        """
-        if dist == 0:
-            return
-
-        if dist > 0:  # Left shift
-            for i in range(self._size):
-                if i + dist < self._size:
-                    self.__setitem__(i, self.get_at(i + dist))
-                else:
-                    self.__setitem__(i, 0)  # Fill with 0s
-
-        else:  # Right shift
-            dist = abs(dist)
-            for i in range(self._size - 1, -1, -1):
-                if i - dist >= 0:
-                    self.__setitem__(i, self.get_at(i - dist))
-                else:
-                    self.__setitem__(i, 0)  # Fill with 0s
-
-    def rotate(self, dist: int) -> None:
-        """
-        Make a bit rotation.
-        If dist is positive, perform a left rotation by `dist`.
-        Otherwise perform a right rotation by `dist`.
-        Time complexity for full marks: O(N)
-        """
-        if self._size == 0 or abs(dist) % self._size == 0:
-            return  # No need to rotate if the distance is zero or a multiple of size
-
-        dist = dist % self._size  # Normalize the distance to within the size of the vector
-        if dist < 0:
-            dist += self._size  # Convert negative rotations to positive equivalent
-
-        def get_bit_vector() -> str:
-            return ''.join(str(self.get_at(i)) for i in range(self._size))
-
-        # Perform the rotation by shifting twice: once for the initial rotation, and once to correct the remaining bits
-        rotated_bits = [self.get_at((i - dist) % self._size) for i in range(self._size)]
-        
-        for i in range(self._size):
-            self.__setitem__(i, rotated_bits[i])
-
-    def get_size(self) -> int:
-        """
-        Return the number of *bits* in the list
-        Time complexity for full marks: O(1)
-        """
-        return self._size
+        self.is_reversed = not self.is_reversed
